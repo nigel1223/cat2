ecdbfb18974d85312ce1fcc3d7658660a8f2fd3b
Nigel <nigel.odhiambo@strathmore.edu>
2026-07-03 01:33:18 +0300

    arrays

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Array of objects with the required 'name' property
+const carsArray = [
+    {
+        name: "2022 Eco Sedan",
+        specs: "32,000 miles | Automatic | Hybrid",
+        price: "$21,500",
+        badge: "Just Arrived"
+    },
+    {
+        name: "2020 Family SUV",
+        specs: "45,500 miles | AWD | V6 Engine",
+        price: "$26,900",
+        badge: "Great Deal"
+    },
+    {
+        name: "2019 Sport Coupe",
+        specs: "18,200 miles | Manual | Turbocharged",
+        price: "$29,400",
+        badge: "Certified"
+    }
+];
