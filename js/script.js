e2d5ab54d852c9fde7b26a84568c432bc718fb7c
Nigel <nigel.odhiambo@strathmore.edu>
2026-07-03 01:37:42 +0300

    loop render

--- js/script.js
+++ js/script.js
@@ -19,3 +19,28 @@ const carsArray = [
         badge: "Certified"
     }
 ];
+
+// Target the inventory container
+const gridContainer = document.querySelector('.grid-container');
+
+// Clear the hardcoded HTML cars
+gridContainer.innerHTML = "";
+
+//  forEach loop
+carsArray.forEach(function(car) {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'grid-item';
+    
+
+    cardDiv.innerHTML = `
+        <span class="status-badge">${car.badge}</span>
+        <h3>${car.name}</h3>
+        <p class="specs">${car.specs}</p>
+        <p class="price">${car.price}</p>
+        <button class="add-wishlist-btn" style="margin-top: 10px; padding: 5px; width: 100%; cursor: pointer;">
+             Add to Wishlist
+        </button>
+    `;
+    
+    gridContainer.appendChild(cardDiv);
+});
